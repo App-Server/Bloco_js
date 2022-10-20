edfe05ef35f001c30e0d69c28502df0bd0995cc7
Alcivan Aragao <109984990+CouldDataBase@users.noreply.github.com>
2022-10-19 21:53:52 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Script JS</title>
+</head>
+<body>
+    
+    <!--<p>Before the script...</p>
+
+    <script>
+        alert('Hello, Word');
+    </script>
+
+    <p>...After the script.</p>-->
+
+    <script>
+       /* alert(3+
+        1
+        +2 ); */
+
+       /* let message;
+        
+        message = 'Hello';
+
+        alert(message); */
+
+        /* let user = 'John',
+        age = 25, 
+        message = 'Hello'; */
+
+        /* let message;
+
+        message = 'Hello!';
+
+        message = 'Word!';
+
+        alert(message); */
+
+        /* let hello = 'Hello word!';
+
+        let message;
+
+        alert(hello);
+        alert(message); */
+
+        /* let $ = 1;
+        let _ = 2;
+
+        alert($ + _); */
+
+        /* const myBirthday = '18.04.1982';
+
+        myBirthday = '01.01.2001'; */
+
+        // alert( 1 * 10);
+
+       /* let = name = "John";
+
+        alert( `Hello, ${name}!` );
+
+        alert( `the result is ${1 + 2 }` ); */
+
+        /* let isGreater = 4 > 1;
+
+        alert( isGreater ); */
+
+        // alert("Hello");
+
+        // result = prompt(title, [defaut]);
+
+        let age = prompt('How old are you', 100);
+
+        alert(`You are ${age} years old`);
+
+    </script>
+</body>
+</html>
